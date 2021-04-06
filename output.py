3512469ead076a3bb905b7908d8ed8e1035b1d6b
mobey3 <81130033+mobey3@users.noreply.github.com>
2021-04-06 16:10:53 +0900

    입력

---
+++ output.py
@@ -0,0 +1,31 @@
+#print('hello 파이썬')           (TERMINAL , cls 지우는거)
+#print('파이썬')
+# < 한줄 주석 파이썬 변수    (''' ''' < 여러줄 주석)
+#num = 10
+#str = 'SG워너비'        
+#print(str)
+#print('숫자: ', num)
+#print('문자열: ', str)
+#print('숫자: %d'% num)
+#print('문자열: %s'% str)
+#PI = 3.14152
+#print('실수: %.2f'% PI)
+
+#변수를 사용해서 아래와 동일하게 출력
+# -----------------
+#개인 주소록 
+# -----------------
+#name = '김진수'
+#i = '남'
+#o = '31살'
+#n = '대구 북구 성북로50'
+#m = '자전거 타기'        
+#print('1. 이름: ', name)  #print('1. 이름: %s'%name)
+#print('2. 성별: ', i)     # %s 문자열 %d 숫자열
+#print('3. 나이: ', o)
+#print('4. 주소: ', n)
+#print('5. 취미: ', m)
+#print('이름:{0}, 성별:{1}, 나이:{2}'.format(name , i , o))
+#print('이름:%s, 성별:%s, 나이:%s'%(name,i,o))
+#print(name, i, o, n, m ,sep=',')
+#print(name, i , o, n, m, sep='/', end='!!')
